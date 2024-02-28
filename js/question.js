fe26304985d6c837a08fd1a0f1cc19acd54d9dd0
naghma12 <156614792+naghma12@users.noreply.github.com>
2024-02-28 20:35:51 +0500

    Add files via upload

---
+++ js/question.js
@@ -0,0 +1,91 @@
+var questions = [
+{
+	"question" : "HTML Stands for?",
+	"option1" : "Hyper Text Markup Language",
+	"option2" : "Hyper Tick Markup Language",
+	"option3" : "Hello Text Markup Language",
+	"option4" : "Hyper Text Module Language",
+	"answer" : "1"
+},
+
+{
+	"question" : "CSS Stands for?",
+	"option1" : "Camera Style Sheet",
+	"option2" : "Co Style Sheet",
+	"option3" : "Cascading Style Sheet",
+	"option4" : "Coordinate Sign Sheet",
+	"answer" : "3"
+},
+
+{
+	"question" : "JS Stands for?",
+	"option1" : "Join Sheet",
+	"option2" : "Javascript",
+	"option3" : "Script Java",
+	"option4" : "Non of the Above",
+	"answer" : "2"
+},
+
+{
+	"question" : "OOP Stands for?",
+	"option1" : "Online Object Protocol",
+	"option2" : "Object Oriented Protocol",
+	"option3" : "Object Oriented Programming",
+	"option4" : "Non of the Above",
+	"answer" : "3"
+},
+
+{
+	"question" : "Select Correct Syntax?",
+	"option1" : "for(var i=0; i++)",
+	"option2" : "for(i=0; i<10; i++)",
+	"option3" : "for(var i=0; i<10; i++)",
+	"option4" : "Both B and C",
+	"answer" : "3"
+},
+
+{
+	"question" : "First Computer Virus is known as?",
+	"option1" : "Rabbit",
+	"option2" : "Creeper Virus",
+	"option3" : "Elk Cloner",
+	"option4" : "SCA Virus",
+	"answer" : "2"
+},
+
+{
+	"question" : "Firewall in Computer is used for?",
+	"option1" : "Security",
+	"option2" : "Data Transmission",
+	"option3" : "Authentication",
+	"option4" : "Monitoring",
+	"answer" : "1"
+},
+
+{
+	"question" : "Which of the Following is not an Operating System?",
+	"option1" : "DOS",
+	"option2" : "Mac",
+	"option3" : "C",
+	"option4" : "Linux",
+	"answer" : "3"
+},
+
+{
+	"question" : "Mac Operating System is Developed by?",
+	"option1" : "Apple",
+	"option2" : "IBM",
+	"option3" : "Microsoft",
+	"option4" : "Samsung",
+	"answer" : "1"
+},
+
+{
+	"question" : ".gif is an Extension of?",
+	"option1" : "Image File",
+	"option2" : "Video File",
+	"option3" : "Audio File",
+	"option4" : "Non of the Above",
+	"answer" : "1"
+},
+];
